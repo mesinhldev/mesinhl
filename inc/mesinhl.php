a2602575fcfd20b7e6409423ca6004ba133c0e43
Mesin HL Development <web.mesinhl@gmail.com>
2023-01-31 11:31:12 +0700

    Update mesinhl.php

--- inc/mesinhl.php
+++ inc/mesinhl.php
@@ -103,3 +103,15 @@ remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
 remove_action('admin_print_scripts', 'print_emoji_detection_script');
 remove_action('admin_print_styles', 'print_emoji_styles');
+
+/**
+ * Remove annoying Elementor license notice
+ */
+add_action("admin_enqueue_scripts", function() {
+	?>
+	<style>
+		.e-notice--extended.e-notice--dismissible.e-notice.notice {
+			display: none !important;
+		}
+	</style><?php
+});
